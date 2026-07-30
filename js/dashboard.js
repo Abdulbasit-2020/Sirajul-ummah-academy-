1ff3b93533388c257edb18197a2ee219a1dce3ad
AbdulBasit <badmusabdulbasit2020@gmail.com>
2026-07-30 13:20:09 +0100

    Implement user authentication state handling

---
+++ js/dashboard.js
@@ -0,0 +1,22 @@
+import { auth } from "./firebase.js";
+
+import {
+onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+
+document.getElementById("username").innerHTML=
+
+"Welcome, "+user.displayName;
+
+}else{
+
+window.location="login.html";
+
+}
+
+});
