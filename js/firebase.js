32b551fda2f039176bb6025333df9817deb479eb
AbdulBasit <badmusabdulbasit2020@gmail.com>
2026-07-30 13:33:54 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,24 +1,17 @@
 
-// Firebase configuration
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-import {
-getAuth
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-import {
-getFirestore
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-// Replace these with your Firebase project's values
 const firebaseConfig = {
-apiKey: "",
-authDomain: "",
-projectId: "",
-storageBucket: "",
-messagingSenderId: "",
-appId: ""
+  apiKey: "AIzaSyAK15eZoNWGaX1YsXeXq33kALfm-sgBX8A",
+  authDomain: "sirajul-ummah-academy.firebaseapp.com",
+  projectId: "sirajul-ummah-academy",
+  storageBucket: "sirajul-ummah-academy.firebasestorage.app",
+  messagingSenderId: "767042103810",
+  appId: "1:767042103810:web:881e896d8b7bf78a9b951b"
 };
 
 const app = initializeApp(firebaseConfig);
